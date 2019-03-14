1703664ac070af68e62a78480a47f34b633718e7
kpilszak <klaudia.pilszak@gmail.com>
2019-03-14 14:08:28 +0100

    Add actionPerformed()

--- src/main/java/ChatFrame.java
+++ src/main/java/ChatFrame.java
@@ -1,8 +1,12 @@
+import lombok.Data;
+
 import javax.swing.*;
-import lombok.Getter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @Data
-public class ChatFrame {
+public class ChatFrame extends Frame implements ActionListener {
 
     private String chatFrameName;
     private JFrame frame = new JFrame();
@@ -20,8 +24,13 @@ public class ChatFrame {
     public void drawChatFrame() {
         frame.getContentPane().add(btnSend);
         frame.getDefaultCloseOperation();
-        frame.setSize(300, 300);
+        frame.setSize(500, 500);
         frame.setTitle(chatFrameName);
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String appendText = txtArea.getText() + "\n" + "You: " + txtField.getText();
+        txtArea.setText(appendText);
     }
 }
