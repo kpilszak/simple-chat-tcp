d844cdc22765bce3f86d8116474816e52261a4eb
kpilszak <klaudia.pilszak@gmail.com>
2019-03-14 12:45:16 +0100

    Add simple GUI

--- src/ChatFrame.java
+++ src/ChatFrame.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+
 public class ChatFrame {
+
+    private String chatFrameName;
+    private JFrame frame = new JFrame();
+    private JButton btnSend = new JButton("Send");
+
+
+    public ChatFrame(String chatFrameName) {
+        this.chatFrameName = chatFrameName;
+    }
+
+
+
+    public void drawChatFrame() {
+        frame.getContentPane().add(btnSend);
+        frame.getDefaultCloseOperation();
+        frame.setSize(300, 300);
+        frame.setTitle(chatFrameName);
+        frame.setVisible(true);
+    }
 }
---
+++ src/Server.java
@@ -0,0 +1,7 @@
+public class Server {
+
+    public static void main(String[] args) {
+        ChatFrame chatFrame = new ChatFrame("Server");
+        chatFrame.drawChatFrame();
+    }
+}
